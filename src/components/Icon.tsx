8c2363716a46855bdee8fc8ec0f6e24b86d3bed9
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-11-24 15:48:56 +0000

    Icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Icon.scss";
+
+interface IconProps {
+  type: "ROUNDED" | "SQUARE" | "CIRCLE";
+  src: string;
+  size: number;
+  padding?: number;
+  name?: string;
+  onClick?: Function;
+}
+
+const Icon = (props: IconProps) => (
+  <div
+    className="icon-wrapper"
+    onClick={() => props.onClick && props.onClick()}
+  >
+    <div className={`icon-container ${props.onClick ? "pointer" : ""}`}>
+      <div
+        className={`icon ${props.type.toLowerCase()}`}
+        style={{
+          padding: props.padding ? `${props.padding}px` : "10px",
+          width: props.size ? `${props.size}px` : "100px",
+        }}
+      >
+        <img alt="" aria-hidden src={props.src} className="background" />
+        <img alt={`icon-${props.name}`} src={props.src} width="100%" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Icon;
