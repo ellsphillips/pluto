ecd96c5ea2df2b221bb7954b291dba376955576e
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-11-25 11:53:26 +0000

    Import styles into component files

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React from "react";
+
 import logo from "./img/logo.svg";
+import "./styles/style.scss";
 import "./styles/App.scss";
+
 import Icon from "./components/Icon";
 import RadioButton from "./components/RadioButton";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      style={{
+        marginTop: "4rem",
+        width: "100%",
+      }}
+    >
       <header className="App-header">
         <Icon
           type="ROUNDED"
           src={logo}
           size={240}
-          padding={100}
+          padding={40}
           onClick={() => alert("Future router...")}
         />
-
         <a
           className="App-link"
           href="https://github.com/ellsphillips/doctor"
@@ -24,7 +31,6 @@ function App() {
         >
           Doctor
         </a>
-
         <RadioButton label="R1" text="Radio-1" image={logo} />
         <RadioButton label="R2" text="Radio-2" />
         <RadioButton label="R3" text="Radio-3" image={logo} />
